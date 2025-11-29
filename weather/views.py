cad00cca4da1ef6a90a79fe849cca71ebed2a1ac
FunkyDrama <cripswhigga18@gmail.com>
2025-11-29 18:43:29 +0200

    - added extended schema for weather endpoint to accept query params in Swagger.

--- weather/views.py
+++ weather/views.py
@@ -3,12 +3,25 @@ from dataclasses import asdict
 from rest_framework.views import APIView
 from rest_framework.response import Response
 from rest_framework import permissions, status
+from drf_spectacular.utils import extend_schema, OpenApiParameter, OpenApiTypes
 from .services import WeatherAPIClient
 
 
 class WeatherAPIView(APIView):
     permission_classes = (permissions.IsAuthenticated,)
 
+    @extend_schema(
+        parameters=[
+            OpenApiParameter(
+                name="city",
+                description="City name for current weather",
+                required=True,
+                type=OpenApiTypes.STR,
+                location=OpenApiParameter.QUERY,
+            ),
+        ],
+        responses={200: None},
+    )
     def get(self, request):
         city = request.query_params.get("city")
         if not city:
